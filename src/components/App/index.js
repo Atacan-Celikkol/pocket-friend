65647dc04e8bad37529b0f747a9a7124f54161e4
Atacan Çelikkol <atacan@ovidos.com>
2019-10-20 01:30:33 +0300

    Code cleanup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,47 +1,19 @@
 import React from 'react';
+import Expenses from '../Expenses';
 import Header from '../Header/index';
-import Expenses from './../Expenses/index';
+import Incomes from '../Incomes/';
 import './style.scss';
 
 function App() {
   return (
-    <div id="main" class="container">
+    <div id="main" className={ 'container' }>
       <Header />
 
-      <div class="col-lg-12">
-        <h3 class="text-success">
-          Incomes
-        </h3>
-
-        <table class="table table-hover table-bordered">
-          <thead class="bg-success text-light">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Date</th>
-              <th scope="col">Description</th>
-              <th scope="col">Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>1</th>
-              <td>01.10.2019</td>
-              <td>Salary</td>
-              <td>₺5000</td>
-            </tr>
-            <tr>
-              <th>2</th>
-              <td>15.10.2019</td>
-              <td>Rent Income</td>
-              <td>₺1200</td>
-            </tr>
-          </tbody>
-        </table>
-
+      <div className={ 'col-lg-12' }>
+        <Incomes />
         <Expenses />
-
-
       </div>
+
     </div >
   );
 }
