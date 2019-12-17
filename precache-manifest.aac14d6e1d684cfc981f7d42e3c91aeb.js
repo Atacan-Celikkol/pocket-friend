cd09da638d572b3b0d803fcac0a4f3decd7f9336
Atacan <atacan.celikkol@hotmail.com>
2019-12-17 16:37:57 +0300

    GH Pages

---
+++ precache-manifest.aac14d6e1d684cfc981f7d42e3c91aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e70f7cefbbeb888bf1d5909eb4585c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "560d5c299e61248806b0",
+    "url": "/static/css/2.30861e7f.chunk.css"
+  },
+  {
+    "revision": "6d2776fd0f743335a1c9",
+    "url": "/static/css/main.cc9485a6.chunk.css"
+  },
+  {
+    "revision": "560d5c299e61248806b0",
+    "url": "/static/js/2.d67f09cd.chunk.js"
+  },
+  {
+    "revision": "6d2776fd0f743335a1c9",
+    "url": "/static/js/main.147e2c5b.chunk.js"
+  },
+  {
+    "revision": "75c3ba08ac7718ee394f",
+    "url": "/static/js/runtime-main.8e5c13cb.js"
+  }
+]);
